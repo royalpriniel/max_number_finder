cf542e4f9772add1fc65f95e8a74cb824cd0b333
Daniel Somtoochi Dav-Emmanuel <74049793+royalpriniel@users.noreply.github.com>
2026-05-18 14:39:07 +0100

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,5 @@
 import {createRoot} from 'react-dom/client';
-import {MaxNumberFinder} from './maxNumberFinder';
+import MaxNumberFinder from './maxNumberFinder';
 
 const root = createRoot(document.getElementById('root'));
 root.render(<MaxNumberFinder />);
