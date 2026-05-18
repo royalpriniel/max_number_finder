18a0bebddfc37bfdad7347a2d6255edda1ce3eea
Daniel Somtoochi Dav-Emmanuel <74049793+royalpriniel@users.noreply.github.com>
2026-05-18 14:27:43 +0100

    Rename GuestList to MaxNumberFinder in index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,5 @@
 import {createRoot} from 'react-dom/client';
-import {GuestList} from './maxNumberFinder';
+import {MaxNumberFinder} from './maxNumberFinder';
 
 const root = createRoot(document.getElementById('root'));
 root.render(<MaxNumberFinder />);
